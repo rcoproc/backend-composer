e6089b5ef9e87fa72d0f05c815d55f1941d3668a
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-03-02 06:57:19 -0400

    Initial Devise Authorization Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  layout :set_layout
+
+  def set_layout
+    if devise_controller? and !user_signed_in?
+      "login"
+    else
+      "application"
+    end
+  end
 end
